886bccfaa1c9a8686ce2fa5e1e93a9e382416a9a
SamaelMarjan <47913328+SamaelMarjan@users.noreply.github.com>
2022-06-13 10:44:56 +0600

    Add files via upload

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero () {
+    return (
+        <section className="hero">
+            <img className="hero--photo" src="../images/air-bnb-grid.png"/>
+            <h2 className="hero--header">Online Experiences</h2>
+            <h4 className="hero--text">Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</h4>
+        </section>
+    )
+}
